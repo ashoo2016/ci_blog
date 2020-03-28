03a32559a7e1dc91281911ec9c17fb159b44b2d1
ashoo2016 <ashoopatna@gmail.com>
2020-03-28 16:10:39 +0530

    All Files Added

---
+++ application/views/pages/home.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>Welcome To The CI Blog Apllication</p>
---
+++ application/views/posts/index.php
@@ -0,0 +1,9 @@
+<h2><?= $title ?></h2>
+<?php foreach($posts as $post) : ?>
+<h3><?php echo $post['title']; ?></h3>
+<small class="post-date">Posted On:<?php echo $post['created_at']; ?></small><br/>
+<?php echo $post['body']; ?>
+<br><br>
+<p><a class="btn btn-secondary" href="<?php echo site_url('/posts/'.$post['slug']);?>">
+	ReadMore</a></p>
+<?php endforeach; ?>
